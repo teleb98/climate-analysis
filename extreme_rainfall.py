4a314affc2718325414c5280a6b7c0a72d7ef2e0
teleb98 <132375457+teleb98@users.noreply.github.com>
2024-02-28 08:07:09 +0900

    Update extreme_rainfall.py

--- extreme_rainfall.py
+++ extreme_rainfall.py
@@ -1,32 +1,57 @@
 import pandas as pd
 import os
 import glob
+from tqdm import tqdm
 
-# 폴더 경로 설정 (여기서는 실제 환경에 맞게 수정해야 함)
-folder_path = "C:/test files/climate analysis/data/rainfall raw data/output/test"
+# 폴더 경로 설정
+folder_path = r"C:\test files\climate analysis\data\rainfall raw final\output\extreme rain"
 
+"""
 # 해당 폴더 내의 모든 CSV 파일을 찾음
 csv_files = glob.glob(os.path.join(folder_path, "*.csv"))
 
+# 빈 데이터프레임 생성
+all_data = pd.DataFrame()
+
 for file in csv_files:
     # 파일 읽기
-    df = pd.read_csv(file, encoding='utf-8-sig')  # 필요에 따라 인코딩 조정
+    df = pd.read_csv(file)
     
-    # 3시간 강수량 합산 계산
-    df['3hr_Rainfall_Sum'] = df.groupby(['Station Name', 'Year', 'Month', 'Day'])['Rainfall'].transform(lambda x: x.rolling(window=3, min_periods=1).sum())
+    # Rainfall 컬럼의 값이 0인 행 제외
+    df_filtered = df[df['Rainfall'] > 0]
     
-    # 극한 호우 조건에 따라 '극한 호우' 컬럼 생성
-    df['Extreme Rainfall'] = ((df['Rainfall'] >= 50) & (df['3hr_Rainfall_Sum'] >= 90)) | (df['Rainfall'] >= 72)
-    df['Extreme Rainfall'] = df['Extreme Rainfall'].map({True: '예', False: '아니오'})
-    
-    # 파일 이름 분리 (확장자 제외)
-    base_name = os.path.basename(file)
-    name_part = os.path.splitext(base_name)[0]
-    
-    # 변경된 데이터를 새 파일로 저장 (원본 파일 이름에 "_updated" 접미사 추가)
-    updated_file_name = f"{name_part}_updated.csv"
-    updated_file_path = os.path.join(folder_path, updated_file_name)
+    # 필터링된 데이터를 all_data에 추가
+    all_data = pd.concat([all_data, df_filtered], ignore_index=True)
 
-    df.to_csv(updated_file_path, index=False, encoding='utf-8-sig')  # 한글을 포함할 경우 'utf-8' 인코딩 사용    
+# 병합된 데이터를 새 파일로 저장
+output_path = os.path.join(folder_path, "extreme_rainfall.csv")
+all_data.to_csv(output_path, index=False, encoding='utf-8-sig')
 
-print("모든 파일이 성공적으로 업데이트되었습니다.")
+print("모든 데이터가 성공적으로 병합되어 'extreme_rainfall.csv' 파일에 저장되었습니다.")
+"""
+
+# 해당 폴더 내의 모든 xlsx 파일을 찾음
+xlsx_files = glob.glob(os.path.join(folder_path, "*.xlsx"))
+
+# 빈 데이터프레임 생성
+all_data = pd.DataFrame()
+
+# progress bar 생성
+for file in xlsx_files:
+    # xlsx 파일 읽기
+    df = pd.read_excel(file) 
+    
+    # Rainfall 컬럼의 값이 0인 행 제외
+    df_filtered = df[df['Rainfall'] > 0]
+    
+    # 필터링된 데이터를 all_data에 추가
+    all_data = pd.concat([all_data, df_filtered], ignore_index=True)
+    
+    # progress bar 업데이트
+    tqdm.write(f"{file} 파일이 필터링되었습니다.")
+    
+# 병합된 데이터를 새 xlsx 파일로 저장
+output_path = os.path.join(folder_path, "extreme_rainfall.xlsx")
+all_data.to_excel(output_path, index=False)
+
+print("모든 데이터가 성공적으로 병합되어 'extreme_rainfall.xlsx' 파일에 저장되었습니다.")
